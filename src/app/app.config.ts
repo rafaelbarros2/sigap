2c78ef1523188abdf4b42a75ac2f244502244df0
unknown <rafael.eng.mecanic@gmail.com>
2025-09-22 11:21:47 -0300

    estrutura inicial

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,19 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import Aura from '@primeuix/themes/aura';
 
 import { routes } from './app.routes';
 
+import { providePrimeNG } from 'primeng/config';
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes),
+    providePrimeNG({
+            theme: {
+                preset: Aura
+            }
+        })
+  ]
 };
+
+
